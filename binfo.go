665d28a891a824acb31befbb87aefef4b4839fe2
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-05-12 00:58:07 +0900

    Modernize codes

--- binfo.go
+++ binfo.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"debug/buildinfo"
 	"go/build"
+	"io/fs"
+	"iter"
+	"log"
 	"os"
 	"path/filepath"
 	"runtime/debug"
 )
 
 // Gobin returns GOBIN value.
+// If GOBIN is vaialbe, this returns GOPATH/bin instead of it.
 func Gobin() string {
 	gobin := os.Getenv("GOBIN")
 	if gobin != "" {
@@ -38,8 +42,7 @@ func readExeInfo(name string) *ExeInfo {
 }
 
 // List returns list of ExeInfo in GOBIN.
-func List(ctx context.Context) ([]*ExeInfo, error) {
-	dir := Gobin()
+func List(ctx context.Context, dir string) ([]*ExeInfo, error) {
 	entries, err := os.ReadDir(dir)
 	if err != nil {
 		return nil, err
@@ -56,4 +59,102 @@ func List(ctx context.Context) ([]*ExeInfo, error) {
 		list = append(list, readExeInfo(name))
 	}
 	return list, nil
+}
+
+// List2 enumerates all ExeInfo in the directory asynchronously.
+func List2(ctx context.Context, dir string) <-chan *ExeInfo {
+	ch := make(chan *ExeInfo)
+	cctx, cancel := context.WithCancelCause(ctx)
+	go func() {
+		defer close(ch)
+		entries, err := os.ReadDir(dir)
+		if err != nil {
+			cancel(err)
+			return
+		}
+		for _, e := range entries {
+			if err := cctx.Err(); err != nil {
+				cancel(err)
+				return
+			}
+			if e.IsDir() {
+				continue
+			}
+			name := filepath.Join(dir, e.Name())
+			ch <- readExeInfo(name)
+		}
+		cancel(nil)
+	}()
+	return ch
+}
+
+func ReadDir(dir string) (iter.Seq[ExeInfo], error) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		log.Printf("failed to read directory entries at %q", dir)
+		return nil, err
+	}
+	return func(yield func(ExeInfo) bool) {
+		for _, e := range entries {
+			if e.IsDir() {
+				continue
+			}
+			name := filepath.Join(dir, e.Name())
+			info := readExeInfo(name)
+			if !yield(*info) {
+				return
+			}
+		}
+	}, nil
+}
+
+type DirReader struct {
+	path  string
+	dir   *os.File
+	cache []fs.DirEntry
+}
+
+func NewDirReader(path string) (*DirReader, error) {
+	dir, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	return &DirReader{
+		path: path,
+		dir:  dir,
+	}, nil
+}
+
+func (r *DirReader) readDir() (fs.DirEntry, error) {
+	if len(r.cache) == 0 {
+		entries, err := r.dir.ReadDir(64)
+		if err != nil {
+			return nil, err
+		}
+		r.cache = entries
+	}
+	entry := r.cache[0]
+	r.cache = r.cache[1:]
+	return entry, nil
+}
+
+func (r *DirReader) Read() (*debug.BuildInfo, error) {
+	for {
+		entry, err := r.readDir()
+		if err != nil {
+			return nil, err
+		}
+		if !entry.Type().IsRegular() {
+			continue
+		}
+		return buildinfo.ReadFile(filepath.Join(r.path, entry.Name()))
+	}
+}
+
+func (r *DirReader) Path() string {
+	return r.path
+}
+
+func (r *DirReader) Close() error {
+	return r.dir.Close()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/koron-go/binfo
 
-go 1.17
+go 1.23.1
